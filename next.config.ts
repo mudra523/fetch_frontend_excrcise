d05bdda09240930f867f3bd44ec5d7841902112f
mudra523 <mudrakoradia1999@gmail.com>
2025-01-19 04:14:41 -0500

    Chnages

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,8 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    esmExternals: "loose",
-  },
-  webpack: (config: { module: { rules: { test: RegExp; include: RegExp; use: { loader: any; options: { presets: string[][]; }; }; }[]; }; }, { }: any) => {
-    // Add a rule to transpile rc-util package
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /node_modules\/rc-util/,
-      use: {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [["next/babel"]],
-        },
-      },
-    });
-    return config;
+  images: {
+    domains: ["frontend-take-home.fetch.com", "images.unsplash.com"],
   },
 };
 
