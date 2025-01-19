871e83a9d30dcddd07c5be3856feca348f13d36d
mudra523 <mudrakoradia1999@gmail.com>
2025-01-19 03:48:01 -0500

    Update next.config.ts for ESM and rc-util transpilation

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     esmExternals: "loose",
   },
-  transpilePackages: ["rc-util"],
+  webpack: (config: { module: { rules: { test: RegExp; include: RegExp; use: { loader: any; options: { presets: string[][]; }; }; }[]; }; }, { }: any) => {
+    // Add a rule to transpile rc-util package
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/rc-util/,
+      use: {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [["next/babel"]],
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
