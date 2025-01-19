565143c2e78b23aca4bab8ba8d1bd47efaeae215
mudra523 <mudrakoradia1999@gmail.com>
2025-01-19 06:16:38 -0500

    Use proxy for dogs search endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+// import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function middleware() {
   const response = NextResponse.next();
-  const origin = request.headers.get('origin') || '*';
+  const origin = '*';
 
   response.headers.set('Access-Control-Allow-Origin', origin);
   response.headers.set('Access-Control-Allow-Credentials', 'true');
