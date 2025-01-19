09863d3851f13d2372df6c8568f21760bcd53e56
mudra523 <mudrakoradia1999@gmail.com>
2025-01-19 03:40:04 -0500

    Changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    esmExternals: "loose",
+  },
+  transpilePackages: ["rc-util"],
 };
 
 export default nextConfig;
