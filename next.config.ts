7c3e404663c86c6e286138e12ff5d9dd70ae6688
mudra523 <mudrakoradia1999@gmail.com>
2025-01-19 05:50:19 -0500

    Use proxy for dogs search endpoint

--- next.config.ts
+++ next.config.ts
@@ -5,20 +5,6 @@ const nextConfig: NextConfig = {
     domains: ["frontend-take-home.fetch.com", "images.unsplash.com", "fetch-frontend-excrcise.vercel.app", "fetch-frontend-excrcise-git-main-mudra523s-projects.vercel.app", "fetch-frontend-excrcise-qg1y5kxkf-mudra523s-projects.vercel.app"],
   },
   transpilePackages: ["rc-util", "rc-picker"],
-  async headers() {
-    return [
-        {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-                { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-        }
-    ]
-}
 };
 
 export default nextConfig;
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -143,7 +143,6 @@ const HomePage: React.FC = () => {
         background: "linear-gradient(to bottom right, #071e3d, #1f4287)",
       }}
     >
-      {/* {console.log("Home", dogs)} */}
       <Sidebar
         breeds={breedList}
         selectedBreed={breed}
